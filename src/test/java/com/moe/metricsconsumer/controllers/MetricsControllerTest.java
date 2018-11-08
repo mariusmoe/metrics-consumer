1521ada45812a5a933920bddd949cb71dcb2f690
Marius oscar moe <mariusomoe@gmail.com>
2018-11-08 19:51:44 +0100

    Add some tests

--- src/test/java/com/moe/metricsconsumer/controllers/MetricsControllerTest.java
+++ src/test/java/com/moe/metricsconsumer/controllers/MetricsControllerTest.java
@@ -17,6 +17,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -24,9 +26,9 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.Assert.*;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -54,7 +56,9 @@ public class MetricsControllerTest {
   @MockBean
   private ResourceServerProperties resourceServerProperties;
 
-  List<MeasureSummary> measureSummary;
+  List<MeasureSummary> measureSummaryList;
+  MeasureSummary measureSummary;
+  MeasureSummary measureSummarySolution;
 
   @Before
   public void setUp() throws Exception {
@@ -68,22 +72,25 @@ public class MetricsControllerTest {
     List<Measure> someMeasure = new ArrayList<>();
     someMeasure.add(new Measure("org.metrics.cyclomatic", someSpecificMeasure));
     someMeasure.add(new Measure("no.hal.javalang", someSpecificMeasure21));
-    measureSummary = new ArrayList<>();
-    measureSummary.add(new MeasureSummary("001","Account-oppgave", "stateandbehavior.Account", someMeasure));
+    measureSummaryList = new ArrayList<>();
+    measureSummaryList.add(new MeasureSummary("001","Account-oppgave", "stateandbehavior.Account", someMeasure));
+
+    measureSummary = new MeasureSummary("001","Account-oppgave", "stateandbehavior.Account", someMeasure);
+    measureSummarySolution = new MeasureSummary("001", true,"Account-oppgave", "stateandbehavior.Account", someMeasure);
 
 
   }
 
   @After
   public void tearDown() throws Exception {
-    measureSummary.clear();
+    measureSummaryList.clear();
   }
 
   @Test
-  public void getAllMeasureSummaries()  throws Exception {
+  public void getAllMeasureSummaries() throws Exception {
     given(this.measureRepository.findAllByUserId("001"))
       .willReturn(
-        measureSummary
+        measureSummaryList
       );
     this.mockMvc.perform(get("/api/"))
       .andExpect(status().isOk())
@@ -92,26 +99,66 @@ public class MetricsControllerTest {
   }
 
   @Test
-  public void getMeasureSummary() {
+  public void getMeasureSummary() throws Exception {
+    given(this.measureRepository.findFirstByUserIdAndTaskId("001", "stateandbehavior.Account"))
+      .willReturn(
+        measureSummary
+      );
+    this.mockMvc.perform(get("/api/stateandbehavior.Account"))
+      .andExpect(status().isOk())
+      .andExpect(content().json("{\"id\":null,\"userId\":\"001\",\"taskName\":\"Account-oppgave\",\"taskId\":\"stateandbehavior.Account\",\"measures\":[{\"measureProvider\":\"org.metrics.cyclomatic\",\"specificMeasures\":[{\"name\":\"cyclomatic Complexity\",\"value\":7.0}]},{\"measureProvider\":\"no.hal.javalang\",\"specificMeasures\":[{\"name\":\"for\",\"value\":7.0},{\"name\":\"foreach\",\"value\":4.0},{\"name\":\"while\",\"value\":1.0}]}],\"solutionManual\":false}"))
+      .andDo(MockMvcResultHandlers.print());
+
   }
 
   @Test
-  public void getSolutionMeasureSummary() {
+  public void getSolutionMeasureSummary() throws Exception {
+    Query query = new Query();
+    query.addCriteria(Criteria.where("isSolutionManual").is(true));
+    query.addCriteria(Criteria.where("taskId").is("stateandbehavior.Account"));
+
+
+    given(this.mongoTemplate.findOne(query, MeasureSummary.class))
+      .willReturn(
+        measureSummarySolution
+      );
+    this.mockMvc.perform(get("/api/solution/stateandbehavior.Account"))
+      .andExpect(status().isOk())
+      .andExpect(content().json("{\"id\":null,\"userId\":\"001\",\"taskName\":\"Account-oppgave\",\"taskId\":\"stateandbehavior.Account\",\"measures\":[{\"measureProvider\":\"org.metrics.cyclomatic\",\"specificMeasures\":[{\"name\":\"cyclomatic Complexity\",\"value\":7.0}]},{\"measureProvider\":\"no.hal.javalang\",\"specificMeasures\":[{\"name\":\"for\",\"value\":7.0},{\"name\":\"foreach\",\"value\":4.0},{\"name\":\"while\",\"value\":1.0}]}],\"solutionManual\":true}"))
+      .andDo(MockMvcResultHandlers.print());
   }
 
   @Test
-  public void newMeasureSummary() {
+  public void newMeasureSummary() throws Exception  {
+    given(this.measureRepository.findFirstByUserIdAndTaskId("001", "stateandbehavior.Account"))
+      .willReturn(
+        measureSummary
+      );
+    this.mockMvc.perform(get("/api/stateandbehavior.Account"))
+      .andExpect(status().isOk())
+      .andExpect(content().json("{\"id\":null,\"userId\":\"001\",\"taskName\":\"Account-oppgave\",\"taskId\":\"stateandbehavior.Account\",\"measures\":[{\"measureProvider\":\"org.metrics.cyclomatic\",\"specificMeasures\":[{\"name\":\"cyclomatic Complexity\",\"value\":7.0}]},{\"measureProvider\":\"no.hal.javalang\",\"specificMeasures\":[{\"name\":\"for\",\"value\":7.0},{\"name\":\"foreach\",\"value\":4.0},{\"name\":\"while\",\"value\":1.0}]}],\"solutionManual\":false}"))
+      .andDo(MockMvcResultHandlers.print());
   }
 
   @Test
-  public void newSolutionMeasureSummary() {
+  public void newSolutionMeasureSummary() throws Exception {
+    given(saveMeasureSummary(measureSummary))
+      .willReturn(
+        measureSummary
+      );
+    this.mockMvc.perform(post("/api/stateandbehavior.Account"))
+      .andExpect(status().isOk())
+      .andExpect(content().json(":\"foreach\",\"value\":4.0},{\"name\":\"while\",\"value\":1.0}]}],\"solutionManual\":false}"))
+      .andDo(MockMvcResultHandlers.print());
+  }
+
+
+
+  @Test
+  public void deleteMeasureSummary() throws Exception {
   }
 
   @Test
-  public void deleteMeasureSummary() {
-  }
-
-  @Test
-  public void deleteMeasureSummary1() {
+  public void deleteMeasureSummary1() throws Exception {
   }
 }
