85d740eabc1560903928acf5e1d5964f60abe9b5
Marius oscar moe <mariusomoe@gmail.com>
2018-11-14 12:24:16 +0100

    Add comments and changes from supervisor meeting
    Still working...

--- src/main/java/com/moe/metricsconsumer/controllers/MetricsControllerFv.java
+++ src/main/java/com/moe/metricsconsumer/controllers/MetricsControllerFv.java
@@ -14,8 +14,10 @@ import no.hal.learning.fv.FeatureValued;
 import no.hal.learning.fv.FvFactory;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
 import org.eclipse.emf.ecore.EStructuralFeature;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -35,6 +37,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.security.Principal;
 import java.util.*;
 import java.util.function.Function;
@@ -71,14 +74,26 @@ public class MetricsControllerFv {
     return getOneGenericMeasureFv(taskId,  true);
   }
 
+  /**
+   * Will create a fv based on the fvConfig for the requested task
+   * @param taskId      id of task
+   * @param isSolution  is this the solution manual fv
+   * @param userId      the user to retreive the fv for
+   * @return            a feature list
+   * @throws EntityNotFoundException
+   */
 	private FeatureList getOneGenericMeasureFv(String taskId, boolean isSolution, String... userId) throws EntityNotFoundException {
-    String _userId = (userId.length >= 1) ? userId[0] : "";
 
+	  // TODO: set userId based on the principal
+    // The userId is optional for now
+	  String _userId = (userId.length >= 1) ? userId[0] : "";
 
+    // featureList is the featureList of the student or solution
     FeatureList featureList = FvFactory.eINSTANCE.createFeatureList();
     MeasureSummary measureSummary;
 
 
+    // Based on wheter this is a solution -> get the correct metrics
     if (isSolution) {
       Query query = new Query();
       query.addCriteria(Criteria.where("isSolutionManual").is(true));
@@ -88,43 +103,64 @@ public class MetricsControllerFv {
       measureSummary = this.measureRepository.findFirstByUserIdAndTaskId("001", taskId);
     }
 
+    // Check if measureSummary was found -> return error if not found
    if (measureSummary == null) {
      throw new EntityNotFoundException(MeasureSummary.class, taskId);
 
    }
 
+   // Add the meassureSummary to the featureList object
     for (Measure measure : measureSummary.getMeasures()) {
       for (SpecificMeasure specificMeasure : measure.getSpecificMeasures()) {
         featureList.getFeatures().put(measure.getMeasureProvider()
           .replace(".", "_") + "__" +specificMeasure.getName()
           .replace(".", "_"), (double) specificMeasure.getValue());
-//        System.out.println(measure.getMeasureProvider() + "_" +specificMeasure.getName());
       }
     }
-    System.out.println("----------------------------------------------");
-    System.out.println("----------------------------------------------");
-    // load config from XMI
+
+    // TODO: Retreive this config based on task
+    // Call class that create dummy fv configuration
     ConfigCreator configCreator = new ConfigCreator();
     try {
-      configCreator.create4();
+      configCreator.create();
     } catch (IOException e) {
       e.printStackTrace();
     }
 
+    // Load config from file system
     Resource.Factory.Registry.INSTANCE.getExtensionToFactoryMap().put("xmi", new XMIResourceFactoryImpl());
     ResourceSet resSet = new ResourceSetImpl();
     Resource resource = resSet.getResource(URI.createURI("config.xmi"), true);
 
     // Replace all references to 'other' in config.xmi with 'featureList'
     for (EObject eObject : resource.getContents()) {
+      replaceReference(eObject);
+      Iterator<EObject> iterator = eObject.eAllContents();
+      while (iterator.hasNext()){
+        replaceReference(iterator.next());
+      }
+
       if (eObject instanceof FeatureValued) {
+        System.out.println(eObject.eClass().getEAllStructuralFeatures());
+        System.out.println(eObject.eClass().getEReferences());
+        System.out.println(eObject.eResource().getURI());
+
+
+
         for (EStructuralFeature eStructuralFeature : eObject.eClass().getEAllStructuralFeatures()) {
+          System.out.println(eStructuralFeature.eResource().getAllContents());
+
           if (eStructuralFeature.getName().equals("other")) {
+            System.out.println(eStructuralFeature.getEContainingClass().getEAllStructuralFeatures());
+
+
             eObject.eSet(eStructuralFeature,featureList);
           }
         }
       }
     }
+
+
 
 
     FeatureList calculatedFeatureList = FvFactory.eINSTANCE.createFeatureList();
@@ -171,6 +207,36 @@ public class MetricsControllerFv {
     }
 
 	  return finalFeatureList;
+  }
+
+  private void replaceReference(EObject eObject) {
+    for (EReference eStructuralFeature : eObject.eClass().getEAllReferences()) {
+      System.out.println(eStructuralFeature.eResource().getAllContents());
+      if (eStructuralFeature.isContainer() || eStructuralFeature.isContainment() || !eStructuralFeature.isChangeable()) {
+        continue;
+      }
+      if (eStructuralFeature.isMany()) {
+        EList<? extends EObject> references = (EList<? extends EObject>) eObject.eGet(eStructuralFeature);
+        for (int i = 0; i < references.size(); i++) {
+          EObject referenced = references.get(i);
+          if (referenced.eResource() != eObject.eResource()) {
+            System.out.println(eStructuralFeature.getName() +" -> " + eObject);
+          }
+        }
+      } else {
+        EObject referenced = (EObject) eObject.eGet(eStructuralFeature);
+        if (referenced.eResource() != eObject.eResource()) {
+          System.out.println(eStructuralFeature.getName() +" -> " + eObject);
+
+        }
+      }
+      if (eStructuralFeature.getName().equals("other")) {
+        System.out.println(eStructuralFeature.getEContainingClass().getEAllStructuralFeatures());
+
+
+//        eObject.eSet(eStructuralFeature,featureList);
+      }
+    }
   }
 
   public FeatureList evaluateFeatureValues(FeatureValued features) {
