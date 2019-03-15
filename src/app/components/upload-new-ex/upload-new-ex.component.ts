eb6c05b0e8d4989c7e4301a54aceaa3ecb8b4354
Unknown <mariusomoe@gmail.com>
2019-03-15 16:09:09 +0100

    Additional tweak of queue removal

--- src/app/components/upload-new-ex/upload-new-ex.component.ts
+++ src/app/components/upload-new-ex/upload-new-ex.component.ts
@@ -52,15 +52,16 @@ export class UploadNewExComponent implements OnInit {
 
   onSuccessItem(item: FileItem, response: string, status: number, headers: ParsedResponseHeaders): any {
     let data = JSON.parse(response); //success server response
-    console.log(data);
+    console.log("File upload returned: " + status);
     this.measureService.notifyNewFiles();
-    this.uploader.clearQueue();
+    console.log(this.uploader.queue);
+    this.uploader.queue = [];
 
   }
   onErrorItem(item: FileItem, response: string, status: number, headers: ParsedResponseHeaders): any {
     let error = JSON.parse(response); //error server response
     console.error(error);
-    this.uploader.clearQueue();
+    this.uploader.queue = [];
   }
 
 
